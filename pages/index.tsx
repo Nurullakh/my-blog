bdcc7d0a0e17fd91111e991a8915d95b38268202
Nurullakh <nurullakh.akhmedov@mail.ru>
2022-07-24 12:14:06 +0300

    Добавил scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import PersonList from './components/PersonList/PersonList';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +13,6 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <PersonList />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -23,6 +21,10 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <div className="lol">
+          <div className="kek">dara</div>
+        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
